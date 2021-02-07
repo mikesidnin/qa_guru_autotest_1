1c202ed3ccf652cbcfe215da6c4559f4e4814162
Сиднин Михаил <msidnin@softrust.ru>
2021-02-08 00:08:13 +0300

    Lession 2.2 locators CSS Selectors + XPath

---
+++ src/test/java/SelenideExample.java
@@ -0,0 +1,73 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideExample {
+    String name = "Mike Sidnin",
+           email = "mikhailsidnin@gmail.com",
+           address = "some address",
+           permanent = "some address";
+
+    @Test
+    @DisplayName("clean")
+    void fillFormTestClean(){
+
+        open("https://demoqa.com/text-box");
+
+        // $(by("id, class, type, etc","value"))
+        $(by("id","userName")).val(name);
+
+        //$x("//input[@id='userEmail']")
+        $x("//input[@id='userEmail']").val(email);
+
+        //$(""); - default css selector. For id add "#..."
+        $("#currentAddress").val(address);
+
+        //$("[id='permanentAddress']"); - default css selector. For id add "#..."
+        $("[id='permanentAddress']").val(permanent);
+
+        //$(byClassName("form-control")).val("permanent address");
+
+        //Button click
+        $("#submit").click();
+
+        //Path 1. Found bug here
+        $("#name").shouldHave(text(name));
+        $("#email").shouldHave(text(email));
+        $("#currentAddress").shouldHave(text(address));
+        $("#permanentAddress").shouldHave(text(permanent));
+    }
+    @Test
+    @DisplayName("straightforward")
+    void fillFormTestStraightforward() {
+
+        open("https://demoqa.com/text-box");
+
+        // $(by("id, class, type, etc","value"))
+        $(by("id", "userName")).val(name);
+
+        //$x("//input[@id='userEmail']")
+        $x("//input[@id='userEmail']").val(email);
+
+        //$(""); - default css selector. For id add "#..."
+        $("#currentAddress").val(address);
+
+        //$("[id='permanentAddress']"); - default css selector. For id add "#..."
+        $("[id='permanentAddress']").val(permanent);
+
+        //$(byClassName("form-control")).val("permanent address");
+
+        //Button click
+        $("#submit").click();
+
+        //Path 2
+        $("#output").shouldHave(text(
+                "Name:" + name + "\n" +
+                "Email:" + email + "\n" +
+                "Current Address :" + address + "\n" +
+                "Permananet Address :" + permanent));
+    }
+}
